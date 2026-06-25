328e3fe31825d8c3049828c4b55714d2a7ec32a9
Rafaela-ME <rafaerbs@gmail.com>
2026-06-25 14:46:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const cards = document.querySelectorAll(".card");
+const contador = document.getElementById("contador");
 
+let descobertos = 0;
+
+cards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        if(!card.classList.contains("visitado")){
+
+            card.classList.add("visitado");
+            descobertos++;
+
+            contador.innerHTML =
+                `Você descobriu ${descobertos} de ${cards.length} curiosidades 🌱`;
+
+            if(descobertos === cards.length){
+
+                setTimeout(() => {
+
+                    alert("🎉 Parabéns! Você concluiu todas as curiosidades sobre Agro Forte, Futuro Sustentável!");
+
+                },300);
+
+            }
+
+        }
+
+        card.classList.toggle("flipped");
+
+    });
+
+});
